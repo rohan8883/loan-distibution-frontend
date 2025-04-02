40fe27634d853d709f4e4367f8949a7ff7767b1f
rohan8883 <rohansingh9135@gmail.com>
2025-04-02 17:12:42 +0530

    changes

--- src/pages/auth/login/LoginTitle.tsx
+++ src/pages/auth/login/LoginTitle.tsx
@@ -6,7 +6,7 @@ export default function LoginTitle() {
       {/* logo */}
       <div className="flex justify-center">
         <Image
-          src="/rologo.jpg"
+          src="/pay.png"
           // src="https://images.unsplash.com/photo-1620288627223-53302f4e8c74?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           width={100}
           height={100}
